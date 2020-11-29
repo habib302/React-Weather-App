bb0a361f956381e738926520645aa55fa6cb8687
MD PEAR E HABIB <md.pear.e.habib20@gmail.com>
2020-11-29 07:54:44 +0600

    fixed grid table

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
 
   useEffect(() => {
     Axios
-      .get(`${API.BASE}/weather?q=${city}&appid=${API.KEY}`)
-      // .then(res => res.json())
+      // .get(`${API.BASE}/weather?q=${city}&appid=${API.KEY}`)
+      .get(`${API.BASE}/weather?q=${city}&units=metric&appid=${API.KEY}`)
       .then(res =>{
         console.log(res);
         setData(res.data);
@@ -32,59 +32,96 @@ function App() {
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <div className="mainTitle">
-            <br></br>
-            <h1>React Weather App Using only Hooks</h1>
-            <h2>Please Enter your City </h2>
-            {/* <br></br> */}
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="search-box">
-          <div className="">
-            <br></br>
-            <input 
-              type="text"
-              placeholder="Search....."
-              onChange={e=> setCity(e.target.value)}
-              value={city}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-      {(typeof data.main != "undefined") ? (
-        <div className="row">
-        
-          <div className="col-md-12">
-            <div className="col-md-4">
-              <h3>City Name</h3>
-              <p>{data.name}</p>
+          <div className="card">
+            <div className="card-header card-header-success">
+              <h1 className="card-title">React Weather App Using only Hooks</h1>
+              <h2 className="card-category">Please Enter your City</h2>
+              <br></br>
+              <form className="search-box">
+                <input 
+                  type="text"
+                  placeholder="Search....."
+                  onChange={e=> setCity(e.target.value)}
+                  value={city}
+                />
+              </form>
             </div>
-            <div className="col-md-4">
-              <h3>Temparature</h3>
-              <p>{Math.round(data.main.temp)} </p>
-            </div>
-            <div className="col-md-4">
-              <h3>Feels Like</h3>
-              <p>{Math.round(data.main.feels_like)} </p>
-            </div>
+            {(typeof data.main != "undefined") ? (
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-4">
+                    <h3>City Name</h3>
+                    <p>{data.name}</p>
+                  </div>
+                  <div className="col-md-4">
+                    <h3>Temparature</h3>
+                    <p>{Math.round(data.main.temp)} &#8451;</p>
+                  </div>
+                  <div className="col-md-4">
+                    <h3>Humidity</h3>
+                    <p>{data.main.humidity} %</p>
+                  </div>
+                </div>
+              </div>
+            ) : ('')}
 
-            <div className="col-md-4">
-              <h3>Humidity(%)</h3>
-              <p>{data.main.humidity}</p>
-            </div>
+            {(typeof data.sys != "undefined") ? (
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-4">
+                    <h3>Country code</h3>
+                    <p>{data.sys.country}</p>
+                  </div>
+                  <div className="col-md-4">
+                    <h3>Sunrise Time</h3>
+                    <p>{data.sys.sunrise} UTC</p>
+                  </div>
+                  <div className="col-md-4">
+                    <h3>Sunset Time</h3>
+                    <p>{data.sys.sunset} UTC</p>
+                  </div>
+                </div>
+              </div>
+            ) : ('')}
+
+
+            {(typeof data.wind != "undefined") ? (
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-4">
+                    <h3>Wind Speed</h3>
+                    <p>{data.wind.speed} meter/sec</p>
+                  </div>
+                  <div className="col-md-4">
+                    <h3>Wind Direction</h3>
+                    <p>{data.wind.deg}</p>
+                  </div>
+                  <div className="col-md-4">
+                    <h3>Wind Gust</h3>
+                    <p>{data.wind.gust} meter/sec</p>
+                  </div>
+                </div>
+              </div>
+            ) : ('')}
+
+            {(typeof data.coord != "undefined") ? (
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-4">
+                    <h3>City Longitude Location</h3>
+                    <p>{data.coord.lon}</p>
+                  </div>
+                  <div className="col-md-4">
+                    <h3>City Latitude Location</h3>
+                    <p>{data.coord.lat}</p>
+                  </div>
+                  
+                </div>
+              </div>
+            ) : ('')}
             
-
-            <div className="col-md-4">
-              <h3></h3>
-              <p>{}</p>
-            </div>
           </div>
-      
         </div>
-      ) : ('')}
       </div>
     </div>
   );
